ccc12738f3eda69a0ad23853f689297d6df5b9db
joo5484@gmail.com <joo5484@gmail.com>
2023-10-11 19:27:30 +0900

    주세은 : 제로( 스택)

---
+++ BackJoon/BackJoon/10773 제로.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+typedef struct NodeData
+{
+	int Data;
+
+}Node;
+
+typedef struct ArrayStack
+{
+	int Top;
+	int Sum;
+	Node* Nodes;
+}Stack;
+
+void Fuction(int Num, Stack** _stack)
+{
+	if (Num != 0)
+	{
+
+		(*_stack)->Nodes[(*_stack)->Top].Data = Num;
+		(*_stack)->Top++;
+		(*_stack)->Sum += Num;
+	}
+	else
+	{
+		--(*_stack)->Top;
+		(*_stack)->Sum -= (*_stack)->Nodes[(*_stack)->Top].Data;
+		(*_stack)->Nodes[(*_stack)->Top].Data = NULL;
+
+	}
+
+}
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+
+	int N;
+	int Num;
+
+	std::cin >> N;
+	Stack* Mainstack = NULL;
+	Mainstack = (Stack*)malloc(24 * N);
+	Mainstack->Nodes = (Node*)malloc(8 * N);
+	Mainstack->Top = 0;
+	Mainstack->Sum = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> Num;
+		Fuction(Num, &Mainstack);
+	}
+	std::cout << Mainstack->Sum;
+
+	free(Mainstack->Nodes);
+	free(Mainstack);
+
+	return 0;
+}
